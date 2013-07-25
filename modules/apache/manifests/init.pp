d6007571c6dee766b795792ed92566e603ed052b
Chris Barbour <cbarbour@taos.com>
2013-07-25 09:55:27 +0000

    Split apache init class into subclasses

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -12,51 +12,11 @@ class apache (
     }
   }
 
-  package { 'httpd':
-    ensure => 'installed',
-  }
+  include apache::packages
+  include apache::config
+  include apache::services
 
-  package { 'php':
-    ensure => 'installed',
-  }
-
-  package { 'mod_ssl':
-    ensure => 'installed',
-  }
-
-  file { '/etc/httpd/conf/httpd.conf':
-    content => template('apache/httpd.conf.erb'),
-    owner   => 'root',
-    group   => 'root',
-    require => Package['httpd', 'php', 'mod_ssl'],
-  }
-
-  file { '/etc/php.ini':
-    source => 'puppet:///modules/apache/etc/php.ini',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0444',
-    require => Package['httpd', 'php', 'mod_ssl'],
-  }
-
-  file { '/etc/httpd/conf.d/port8080.conf':
-    source => 'puppet:///modules/apache/etc/httpd/conf.d/port8080.conf',
-    owner  => 'root',
-    group  => 'root',
-    require => Package['httpd', 'php', 'mod_ssl'],
-  }
-
-  file { '/var/www/port8080/':
-    ensure => 'directory',
-    mode   => '0664',
-  }
-
-  service { 'httpd':
-    ensure => 'running',
-    enable => true,
-    subscribe => File['/etc/httpd/conf/httpd.conf',
-                      '/etc/php.ini',
-                      '/etc/httpd/conf.d/port8080.conf',
-                      '/var/www/port8080'],
-  }
+  Class['apache::packages'] ->
+  Class['apache::config'] ~>
+  Class['apache::services']
 }
